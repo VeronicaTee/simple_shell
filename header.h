ee2d4abef3b75a72d7ab83ea7532398930e7e1f1
Veronica <veronicatomilola@gmail.com>
2022-09-03 19:02:36 -0700

    Print the current environment

--- header.h
+++ header.h
@@ -7,6 +7,14 @@ void replace_expr(char *, int, char **);
 
 
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+
+
 
 
 #endif
